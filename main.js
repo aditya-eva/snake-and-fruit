566121091c0d7f31b0884f51d7451e8508f0bc7c
Aditya Mohaty <adityamohaty@evawinoptimize.com>
2026-02-20 17:14:19 +0530

    feat: refactor game logic and enhance fruit generation mechanics along with renderring the snake after a fixed interval

--- main.js
+++ main.js
@@ -1,37 +1,114 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { createBoard } from './board';
-import { addLights } from './lights';
+import { createFruit } from "./fruitRender"
+import { perspectiveCamera } from './core/camera';
+import { addLights } from './core/light';
+import { createBoard } from './gameComponents/board';
+import { renderSnake } from './gameComponents/snake/renderSnake';
+import { makeNewFruit } from './gameComponents/createFruit';
+import renderer from './core/renderer';
+import { restartGame } from './gameComponents/restartGame';
+import { windowResize } from './core/windowResizing';
+
+const gridSize = 20;
+let snake = [{ x: 6, y: 6 }]
+let fruit = { x: 5, y: 5 }
+let direction = { x: 1, y: 0 }
+let nextDirection = { x: 1, y: 0 }
+let score = 0
+let gameOver = false
+let gameStarted = false
+let lastUpdatedTime = 0
+let updateInterval = 300
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x1a1a1a);
+scene.background = new THREE.Color("black");
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(10, 15, 10);
-camera.lookAt(0, 0, 0);
+const camera = perspectiveCamera(gridSize);
+scene.add(camera);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-document.body.appendChild(renderer.domElement);
+renderer
 
 addLights(scene);
-createBoard(scene);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 2;
+createBoard(gridSize, scene);
 
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+const fruitMesh = createFruit(scene);
+fruitMesh.position.set(fruit.x, 0.5, fruit.y);
 
-function animate() {
+let snakeSegments = [];
+const scoreBoard = document.querySelector(".score");
+
+windowResize(camera, renderer);
+
+
+function updateSnake() {
+    // Apply nextDirection to Direction
+    direction = { ...nextDirection };
+    // compute New Head
+    const newHead = {
+        x: snake[0].x + direction.x,
+        y: snake[0].y + direction.y
+    };
+    if(newHead.x < 0 || newHead.x > gridSize || newHead.y < 0 || newHead.y > gridSize ) {
+        gameOver = true;
+        return;
+    }
+    let ans = snake.some((point) => point.x === newHead.x && point.y === newHead.y);
+    if(ans) {
+        gameOver = true;
+        return;
+    }
+    // add new Head at the beggining
+    snake.unshift(newHead);
+    if(newHead.x === fruit.x && newHead.y === fruit.y) {
+        score++;
+        scoreBoard.innerHTML = score;
+        makeNewFruit(gridSize, fruit, fruitMesh, snake)
+    } else {
+        // remove last element
+        snake.pop();
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp' && direction.x !== 0) {
+        nextDirection = { x: 0, y: -1 }
+    } else if (e.key === 'ArrowDown' && direction.x !== 0) {
+        nextDirection = { x: 0, y: 1 }
+    } else if (e.key === 'ArrowLeft' && direction.y !== 0) {
+        nextDirection = { x: -1, y: 0 }
+    } else if (e.key === 'ArrowRight' && direction.y !== 0) {
+        nextDirection = { x: 1, y: 0 }
+    }
+})
+ 
+
+const gameOverDiv = document.getElementById("gameOver");
+const restartBtn = document.getElementById("restartBtn");
+       
+restartBtn.addEventListener('click', () => {
+    restartGame(snakeSegments, snake, direction, nextDirection, scene);
+    score = 0;
+    gameOver = false;
+    gameStarted = false;
+    gameOverDiv.style.display = "none";   
+    scoreBoard.innerText = score;
+})
+
+function animate(currentTime) {
     requestAnimationFrame(animate);
+    fruitMesh.rotation.x += 0.01
+    if (!gameOver && currentTime - lastUpdatedTime > updateInterval) {
+        console.log(updateInterval);
+        updateSnake();
+        renderSnake(snake, snakeSegments, scene);
+        lastUpdatedTime = currentTime;
+    }
+    if(gameOver) {
+        gameOverDiv.style.display = "block";   
+    }
     renderer.render(scene, camera);
 }
-animate();
+
+animate(0);
+renderSnake(snake, snakeSegments, scene);
